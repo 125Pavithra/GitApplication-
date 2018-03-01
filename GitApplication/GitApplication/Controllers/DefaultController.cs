4bf33a2a85e4fc70d2681ddf367839a8ae80652d
125Pavithra <125pavithra@gmail.com>
2018-03-01 10:32:03 +0530

    Initial Commit

---
+++ GitApplication/GitApplication/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GitApplication.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public string Post()
+        {
+            return "Good morning have a nice day";
+        }
+    }
+}
